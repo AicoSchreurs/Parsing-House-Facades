8d54cccef65cd8b80500f2a6b815f7e9941a44b3
kiejaana <kiejaana@gmail.com>
2026-04-16 19:44:12 +0200

    Update install.sh

--- installations/install.sh
+++ installations/install.sh
@@ -2,5 +2,17 @@
 
 set -e
 
+# PyTorch (CUDA 12.1)
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
+
+# General packages
 pip install opencv-python numpy pandas seaborn matplotlib scipy Pillow hydra-core
+
+# Web scraping
+pip install requests selenium
+
+# SAM1
+pip install git+https://github.com/facebookresearch/segment-anything.git
+
+# FastSAM
+pip install ultralytics
