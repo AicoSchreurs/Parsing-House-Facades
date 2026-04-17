3505e14ba4f6830ad7a7a0e4d4fb370282d8ec05
kiejaana <kiejaana@gmail.com>
2026-04-17 15:33:52 +0200

    Update download_ckpts.sh

--- checkpoints/download_ckpts.sh
+++ checkpoints/download_ckpts.sh
@@ -43,6 +43,15 @@ sam2p1_hiera_s_url="${SAM2p1_BASE_URL}/sam2.1_hiera_small.pt"
 sam2p1_hiera_b_plus_url="${SAM2p1_BASE_URL}/sam2.1_hiera_base_plus.pt"
 sam2p1_hiera_l_url="${SAM2p1_BASE_URL}/sam2.1_hiera_large.pt"
 
+# Define the URLs for SAM vit checkpoints
+sam_vit_b_url="https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth"
+sam_vit_l_url="https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth"
+sam_vit_h_url="https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth"
+
+# Define the URLs for FastSAM checkpoints
+fastsam_x="https://github.com/ultralytics/assets/releases/download/v8.4.0/FastSAM-x.pt"
+fastsam_s="https://github.com/ultralytics/assets/releases/download/v8.4.0/FastSAM-s.pt"
+
 # SAM 2.1 checkpoints
 echo "Downloading sam2.1_hiera_tiny.pt checkpoint..."
 $CMD $sam2p1_hiera_t_url || { echo "Failed to download checkpoint from $sam2p1_hiera_t_url"; exit 1; }
@@ -55,5 +64,24 @@ $CMD $sam2p1_hiera_b_plus_url || { echo "Failed to download checkpoint from $sam
 
 echo "Downloading sam2.1_hiera_large.pt checkpoint..."
 $CMD $sam2p1_hiera_l_url || { echo "Failed to download checkpoint from $sam2p1_hiera_l_url"; exit 1; }
+
+# SAM vit checkpoints
+echo "Downloading sam_vit_b_01ec64.pth checkpoint..."
+$CMD $sam_vit_b_url || { echo "Failed to download checkpoint from $sam_vit_b_url"; exit 1; }
+
+echo "Downloading sam_vit_l_0b3195.pth checkpoint..."
+$CMD $sam_vit_l_url || { echo "Failed to download checkpoint from $sam_vit_l_url"; exit 1; }
+
+echo "Downloading sam_vit_h_4b8939.pth checkpoint..."
+$CMD $sam_vit_h_url || { echo "Failed to download checkpoint from $sam_vit_h_url"; exit 1; }
+
+
+# FastSAM checkpoints
+echo "Downloading FastSAM-x.pt checkpoint..."
+$CMD $fastsam_x || { echo "Failed to download checkpoint from $fastsam_x"; exit 1; }
+
+echo "Downloading FastSAM-s.pt checkpoint..."
+$CMD $fastsam_s || { echo "Failed to download checkpoint from $fastsam_s"; exit 1; }
+
 
 echo "All checkpoints are downloaded successfully."
